a6a0d26371d90ec822d62178b1925b8f07f7fe80
Dmitriy <kowalevichdima16@gmail.com>
2019-03-01 15:39:34 +0300

    commit

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const request = require("request");
 const option = {
   parse_mode: "Markdown",
   reply_markup: {
-    one_time_keyboard: true,
+    resize_keyboard:true,
     keyboard: [
       [
         {
-          text: "My location",
-          request_location: true
+          text: "Отправить геопозицию",
+          request_location: true,
+          callback_data: "location"
         }
+      ],
+      [
+
       ]
     ]
   }
@@ -23,48 +27,34 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg, match) => {
   const chatId = msg.chat.id;
-  const message =
-    "Привет, отправьте мне геопозицию с помощью кнопки , чтобы узнать подробные данные о погоде в этом месте!";
-  bot.sendMessage(chatId, message,option);
-});
+  const message ="Привет, отправьте мне геопозицию с помощью кнопки , чтобы узнать подробные данные о погоде в этом месте!";
+  bot.sendMessage(chatId, message, option).then(() => {
+    bot.once("location", msg => {
+      const lat = msg.location.latitude.toFixed(2);
+      const lon = msg.location.longitude.toFixed(2);
+      const cels = 273;
 
-bot.onText(/^\/start/, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.once("location", msg => {
+      request(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b9d7db5b1bd38af3d125a21fe14ead1b`,
+        function(error, response, body) {
+          const data = JSON.parse(body);
 
-    const lat = (msg.location.latitude).toFixed(2);
-    const lon = (msg.location.longitude).toFixed(2);
+          let ms = `
+          *Погода в ${data.name}:*
+          ${data.weather[0].main}
+          Температура: ${(data.main.temp - cels).toFixed(1)}℃
+          Давление: ${data.main.pressure}
+          Влажность: ${data.main.humidity}
+          Минимальная температура: ${(data.main.temp_min - cels).toFixed(1)}℃
+          Максимальная температура: ${(data.main.temp_max - cels).toFixed(1)}℃
+          Скорость ветра: ${data.wind.speed}
+          Направление ветра: ${data.wind.deg}°
+          Процент облачности: ${data.clouds.all}
+          `;
 
-    request(
-      'https://api.openweathermap.org/data/2.5/weather?lat=53.89&lon=27.56&appid=b9d7db5b1bd38af3d125a21fe14ead1b',
-      function(error,response,body){
-
-        const data = JSON.parse(body);
-        
-        let ms = `
-        *Погода в ${data.name}:*
-        ${data.weather[0].main}
-        Температура: ${data.main.temp}
-        Давление: ${data.main.pressure}
-        Влажность: ${data.main.humidity}
-        Минимальная температура: ${data.main.temp_min}
-        Максимальная температура: ${data.main.temp_max}
-        Скорость ветра: ${data.wind.speed}
-        Направление ветра: ${data.wind.deg}°
-        Процент облачности: ${data.clouds.all}
-
-
-
-        `
-
-        bot.sendMessage(
-          chatId,
-          ms + [lat, lon].join(","),
-          option
-        );
-      }
-      
-    );
-    
+          bot.sendMessage(chatId, ms, option);
+        }
+      );
+    });
   });
 });
